df8ebe9e98ad227522a96d82d91c2246efa0666d
Sam Cappleman-Lynes <sam.cappleman-lynes@softwire.com>
2019-07-25 10:18:36 +0100

    Added some example logs

--- src/main/java/com/softwire/training/gasmon/GasMon.java
+++ src/main/java/com/softwire/training/gasmon/GasMon.java
@@ -1,11 +1,18 @@
 package com.softwire.training.gasmon;
 
 import com.softwire.training.gasmon.config.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GasMon {
 
+    private static final Logger LOG = LoggerFactory.getLogger(GasMon.class);
+
     public static void main(String[] args) {
+        LOG.info("Reading config file...");
         Config config = new Config();
+
+        LOG.info("Greeting my user!");
         System.out.println(config.example.greeting);
     }
 
